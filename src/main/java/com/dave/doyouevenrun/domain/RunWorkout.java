4b23ff27c8e8d99479a71cf231f59d05ca5524f0
Dave Harms <daveharmswebdev@gmail.com>
2022-02-20 20:46:56 -0600

    refactored RunWorkout.java for lombok

--- src/main/java/com/dave/doyouevenrun/domain/RunWorkout.java
+++ src/main/java/com/dave/doyouevenrun/domain/RunWorkout.java
@@ -1,5 +1,9 @@
 package com.dave.doyouevenrun.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +15,9 @@ import javax.persistence.Id;
  * Time: 10:53 AM
  */
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class RunWorkout {
 
     @Id
@@ -20,36 +27,4 @@ public class RunWorkout {
     private String title;
     private Long distance;
     private String time;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Long getDistance() {
-        return distance;
-    }
-
-    public void setDistance(Long distance) {
-        this.distance = distance;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
 }
